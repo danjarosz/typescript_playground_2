d7a5d55dcffbaa0b63f53097c2f985a479352b9a
danjarosz <jarosz.daniel@protonmail.com>
2021-01-18 21:22:53 +0100

    feat: types

--- app.ts
+++ app.ts
@@ -1,1 +1,17 @@
-console.log("Your code goes here...");
+function add(n1: number, n2: number, printResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (printResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const num1 = 5;
+const num2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+const result = add(num1, num2, false, "");
+console.log(result);
+add(num1, num2, printResult, resultPhrase);
