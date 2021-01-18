f1736a04bfc9da8c6656cbe76af601c51dd6f3a9
danjarosz <jarosz.daniel@protonmail.com>
2021-01-18 22:49:26 +0100

    feat: tuple

--- app.ts
+++ app.ts
@@ -2,12 +2,14 @@ interface Person {
   name: string;
   age: number;
   hobbies: string[];
+  role: [number, string]; //tuple
 }
 
 const person: Person = {
   name: "Daniel",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
 let favoriteActivites: any[];
@@ -16,5 +18,7 @@ favoriteActivites = ["Sports", 1];
 for (const hobby of person.hobbies) {
   console.log(hobby.toUpperCase());
 }
+
+person.role[0] = 1;
 
 console.log(person.name, person.age);
