ffa1c6704992cac8bef74f2fc6de561e5c3c11a5
danjarosz <jarosz.daniel@protonmail.com>
2021-01-18 21:53:37 +0100

    feat: objects

--- app.ts
+++ app.ts
@@ -1,17 +1,11 @@
-function add(n1: number, n2: number, printResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (printResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
+interface Person {
+  name: string;
+  age: number;
 }
 
-const num1 = 5;
-const num2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
+const person: Person = {
+  name: "Daniel",
+  age: 30,
+};
 
-const result = add(num1, num2, false, "");
-console.log(result);
-add(num1, num2, printResult, resultPhrase);
+console.log(person.age);
