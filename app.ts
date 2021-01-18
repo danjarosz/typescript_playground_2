38022fa0f638f696d2b990d9cb54e712043e9b75
danjarosz <jarosz.daniel@protonmail.com>
2021-01-18 22:00:56 +0100

    feat: arrays

--- app.ts
+++ app.ts
@@ -1,11 +1,20 @@
 interface Person {
   name: string;
   age: number;
+  hobbies: string[];
 }
 
 const person: Person = {
   name: "Daniel",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
-console.log(person.age);
+let favoriteActivites: any[];
+favoriteActivites = ["Sports", 1];
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+console.log(person.name, person.age);
