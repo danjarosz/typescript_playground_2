b649686ce065b2ce9b298b47192818f9695cbb86
danjarosz <jarosz.daniel@protonmail.com>
2021-01-18 23:02:23 +0100

    feat: enum

--- app.ts
+++ app.ts
@@ -1,8 +1,15 @@
+enum RoleVal {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
 interface Person {
   name: string;
   age: number;
   hobbies: string[];
   role: [number, string]; //tuple
+  roleEnum: RoleVal; //enum
 }
 
 const person: Person = {
@@ -10,6 +17,7 @@ const person: Person = {
   age: 30,
   hobbies: ["Sports", "Cooking"],
   role: [2, "author"],
+  roleEnum: RoleVal.ADMIN,
 };
 
 let favoriteActivites: any[];
@@ -20,5 +28,9 @@ for (const hobby of person.hobbies) {
 }
 
 person.role[0] = 1;
+
+if (person.roleEnum === RoleVal.ADMIN) {
+  console.log("Is admin");
+}
 
 console.log(person.name, person.age);
