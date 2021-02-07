f4997b0ab0f2c1626e940a1748d58d93da9b1f6f
danjarosz <jarosz.daniel@protonmail.com>
2021-02-07 17:48:38 +0100

    feat: preivate and public

--- classes_&_interfaces/src/app.ts
+++ classes_&_interfaces/src/app.ts
@@ -1,1 +1,33 @@
-// Code goes here!
+class Department {
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  name: string; // it is public by default
+  private employees: string[] = [];
+
+  describe = () => {
+    console.log(`Department: ${this.name}`);
+  };
+
+  addEmployee = (empolyee: string) => {
+    this.employees.push(empolyee);
+  };
+
+  printEmployeeInformation = () => {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  };
+}
+
+const accounting = new Department("Accounting");
+// console.log(accounting);
+accounting.describe();
+
+// const accountingCopy = { describe: accounting.describe };
+// accountingCopy.describe();
+
+accounting.addEmployee("Jon Snow");
+accounting.addEmployee("Janusz");
+// accounting.employees[2] = "nanna"; // illegal due to private
+accounting.printEmployeeInformation();
