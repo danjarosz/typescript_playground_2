c3401106d8bd849d604a5195ce53f6ecbfafead9
danjarosz <jarosz.daniel@protonmail.com>
2021-02-10 19:05:14 +0100

    feat: extending classes

--- classes_&_interfaces/src/app.ts
+++ classes_&_interfaces/src/app.ts
@@ -25,7 +25,35 @@ class Department {
   };
 }
 
-const accounting = new Department("d1", "Accounting");
+class ITDepartment extends Department {
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+
+  public admins: string[];
+
+  public printAdmins = () => {
+    console.log(this.admins.length);
+    console.log(this.admins);
+  };
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  public addReport = (text: string) => {
+    this.reports.push(text);
+  };
+
+  public printReports = () => {
+    console.log("Reports: ", this.reports);
+  };
+}
+
+const accounting = new AccountingDepartment("d1", []);
 // console.log(accounting);
 accounting.describe();
 
@@ -36,3 +64,12 @@ accounting.addEmployee("Jon Snow");
 accounting.addEmployee("Janusz");
 // accounting.employees[2] = "nanna"; // illegal due to private
 accounting.printEmployeeInformation();
+accounting.addReport("report 1");
+accounting.printReports();
+
+const itDep = new ITDepartment("d2", ["admin1", "admin2"]);
+itDep.describe();
+itDep.addEmployee("Mietak");
+itDep.addEmployee("Robert");
+itDep.printEmployeeInformation();
+itDep.printAdmins();
