65b32d762d3e9f07017766cbb27e21fb6845252c
danjarosz <jarosz.daniel@protonmail.com>
2021-02-11 18:05:30 +0100

    feat: interfaces in classes

--- classes_&_interfaces/src/app.ts
+++ classes_&_interfaces/src/app.ts
@@ -14,3 +14,42 @@ user1 = {
 };
 
 user1.greet("Hello");
+
+//--------------------------
+interface Greetable {
+  name: string;
+  age?: number;
+  greet(phrase: string): void;
+}
+
+class PersonCL implements Greetable {
+  name: string;
+  age: number;
+
+  constructor(n: string, age: number = 30) {
+    this.name = n;
+    this.age = age;
+  }
+
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  }
+}
+
+let user2: Greetable;
+user2 = {
+  name: "Daniel",
+  greet(phrase: string) {
+    console.log(phrase + " " + this.name);
+  },
+};
+
+user1.greet("Hello");
+
+let user3 = new PersonCL("Janko");
+user3.greet("Cześć,");
+console.log(user3);
+
+let user4 = new PersonCL("Ziemowit", 22);
+user4.greet("Cześć,");
+console.log(user4);
