43c5e1f18470f14586a640f3e780b1a255ab3b51
danjarosz <jarosz.daniel@protonmail.com>
2021-02-12 21:18:39 +0100

    feat: intersection type

--- advanced_types/src/app.ts
+++ advanced_types/src/app.ts
@@ -1,1 +1,36 @@
-// Code goes here!
+type Admin = {
+  name: string;
+  privileges: string[];
+};
+
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+//intersection type - definition 1
+type ElevatedEmployee = Admin & Employee;
+
+// //intersection type - definition 2
+// interface Admin {
+//   name: string;
+//   privileges: string[];
+// }
+
+// interface Employee {
+//   name: string;
+//   startDate: Date;
+// }
+
+// interface ElevatedEmployee extends Employee, Admin {}
+
+const e1: ElevatedEmployee = {
+  name: "Daniel",
+  privileges: ["create server"],
+  startDate: new Date(),
+};
+
+type Combine = string | number;
+type Numeric = number | boolean;
+
+type Universal = Combine & Numeric;
