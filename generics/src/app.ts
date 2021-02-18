97f7bc15c560a15b7c35aad5c7d63dd403e3b00a
danjarosz <jarosz.daniel@protonmail.com>
2021-02-18 21:07:12 +0100

    feat: functions

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,14 +1,25 @@
 // INTRODUCTION
-// // example generic types: Array, Promise
-// const names: Array<string> = []; // string[]
-// // names[0].split(" ");
+// example generic types: Array, Promise, Function
+// Array
+const names: Array<string> = []; // string[]
+// names[0].split(" ");
 
-// const promise: Promise<number> = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//     resolve(10);
-//   }, 2000);
-// });
+//Promise
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 2000);
+});
 
 // promise.then((data) => {
 //   // data.split(" ");
 // });
+
+//Function
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+const mergeResult = merge({ name: "dan", surname: "jar" }, { age: 30 });
+console.log(mergeResult);
+const mergeResult2 = merge({ brand: "Seat" }, { model: "Ibiza", age: 5 });
+console.log(mergeResult2);
