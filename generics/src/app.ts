9eb7543fc99324a23478ec32b6a711898b858cb1
danjarosz <jarosz.daniel@protonmail.com>
2021-02-18 17:48:58 +0100

    feat: intro

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,1 +1,14 @@
-// Code goes here!
+// INTRODUCTION
+// // example generic types: Array, Promise
+// const names: Array<string> = []; // string[]
+// // names[0].split(" ");
+
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
+
+// promise.then((data) => {
+//   // data.split(" ");
+// });
