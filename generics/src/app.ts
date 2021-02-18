c475b9b7136a6513a8a2b9f9ea24b61e12a04cb7
danjarosz <jarosz.daniel@protonmail.com>
2021-02-18 22:32:46 +0100

    feat: generic classes

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,10 +1,12 @@
 // INTRODUCTION
-// example generic types: Array, Promise, Function
+// example generic types: Array, Promise, Function, Class
+//--------------------------------------------------
 // Array
 const names: Array<string> = []; // string[]
 // names[0].split(" ");
 
-//Promise
+//--------------------------------------------------
+// Promise
 // const promise: Promise<number> = new Promise((resolve, reject) => {
 //   setTimeout(() => {
 //     resolve(10);
@@ -15,7 +17,8 @@ const names: Array<string> = []; // string[]
 //   // data.split(" ");
 // });
 
-//Function
+//--------------------------------------------------
+// Function
 // function merge<T, U>(objA: T, objB: U) {
 //   return Object.assign(objA, objB);
 // }
@@ -55,3 +58,47 @@ function extractAndConvert<T extends object, U extends keyof T>(
 }
 
 console.log(extractAndConvert({ name: "Dan" }, "name"));
+
+//--------------------------------------------------
+// Class
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    const index = this.data.indexOf(item);
+    if (index === -1) {
+      return;
+    }
+    this.data.splice(index, 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+const numberStorage = new DataStorage<number>();
+
+textStorage.addItem("hello");
+console.log(textStorage.getItems());
+textStorage.addItem("Something new");
+textStorage.removeItem("hello");
+console.log(textStorage.getItems());
+
+numberStorage.addItem(10);
+console.log(numberStorage.getItems());
+numberStorage.addItem(20);
+numberStorage.removeItem(10);
+console.log(numberStorage.getItems());
+
+// const objStorage = new DataStorage<object>();
+// objStorage.addItem({ name: "Dan" });
+// console.log(objStorage.getItems());
+// objStorage.addItem({ name: "Ran" });
+// objStorage.removeItem({ name: "Dan" });
+// console.log(objStorage.getItems());
