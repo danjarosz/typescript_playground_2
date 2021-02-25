fca7e539741a8c0a76da17ecac685ba5aa38ffea
danjarosz <jarosz.daniel@protonmail.com>
2021-02-25 20:14:55 +0100

    fix

--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -150,23 +150,17 @@ interface ValidatorConfig {
 
 const registeredValidators: ValidatorConfig = {};
 
-function Required(target: any, propName: string) {
+function RequiredField(target: any, propName: string) {
   registeredValidators[target.constructor.name] = {
     ...registeredValidators[target.constructor.name],
-    [propName]: [
-      ...registeredValidators[target.constructor.name][propName],
-      "required",
-    ],
+    [propName]: ["required"],
   };
 }
 
 function PositiveNumber(target: any, propName: string) {
   registeredValidators[target.constructor.name] = {
     ...registeredValidators[target.constructor.name],
-    [propName]: [
-      ...registeredValidators[target.constructor.name][propName],
-      "positive",
-    ],
+    [propName]: ["positive"],
   };
 }
 
